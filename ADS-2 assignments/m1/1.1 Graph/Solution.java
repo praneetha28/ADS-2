e3addf7bc7390d71933075adedac402b35bbfa5e
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-29 17:47:03 +0530

    "ads2_m1_a1_1: 3/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m1/1.1 Graph/Solution.java
+++ ADS-2 assignments/m1/1.1 Graph/Solution.java
@@ -26,12 +26,12 @@ public final class Solution {
         Scanner sc = new Scanner(System.in);
         String word = sc.nextLine();
         int n =  sc.nextInt();
-            int m = sc.nextInt();
-            if (n == 0 || m == 0 ) {
-                System.out.println(n + " vertices, " + m + " edges");
-                System.out.println("No edges");
-                return;
-            }
+        int m = sc.nextInt();
+        if (n == 0 || m == 0 ) {
+            System.out.println(n + " vertices, " + m + " edges");
+            System.out.println("No edges");
+            return;
+        }
         switch (word) {
 	        case"List":
 	        sc.nextLine();
@@ -48,6 +48,20 @@ public final class Solution {
 	        }
 	       	System.out.println(gl.toString());
 	        break;
+            case"Word":
+            sc.nextLine();
+            String vtxes = sc.nextLine();
+            String[] tkens = vtxes.split(",");
+            GraphMatrix gm = new GraphMatrix(n);
+            for (int i = 0; i < n; i++) {
+                gm.addVertex(tkens[i]);
+            }
+            for (int i = 0; i < m; i++) {
+                String line = sc.nextLine();
+                String[] tokens = line.split(" ");
+                gm.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+            }
+            System.out.println(gm.toString());
 	        default:
 	        break;
 	    }
