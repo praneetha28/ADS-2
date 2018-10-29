ea7f52656a219a27274f725fae0843546a0417c3
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-29 20:43:53 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m1/1.1 Graph/Bag.java
+++ ADS-2 assignments/m1/1.1 Graph/Bag.java
@@ -6,24 +6,16 @@ import java.util.Iterator;
  * { item_description }
  */
 import java.util.NoSuchElementException;
-
-/**
- *  The <tt>Bag</tt> class represents a bag (or multiset) of
- *  generic items. It supports insertion and iterating over the
- *  items in arbitrary order.
- *  <p>
- *  The <em>add</em>, <em>isEmpty</em>, and <em>size</em>  operation
- *  take constant time. Iteration takes time proportional to the number of items.
- *  <p>
- *  For additional documentation, see
- *  <a href="http://algs4.cs.princeton.edu/13stacks">Section 1.3</a> of
- *  <i>Algorithms, 4th Edition</i> by Robert Sedgewick and Kevin Wayne.
+/**.
+ * Class for bag.
+ *
+ * @param      <Item>  The item
  */
 public class Bag<Item> implements Iterable<Item> {
     /**.
      * { var_description }
      */
-    private int N;
+    private int N_1;
     /**.
      * { var_description }
      */
@@ -49,39 +41,44 @@ public class Bag<Item> implements Iterable<Item> {
         first = null;
         N = 0;
     }
-
-   /**
-     * Is the BAG empty?
-     */
+/**.
+ * Determines if empty.
+ *
+ * @return     True if empty, False otherwise.
+ */
     public boolean isEmpty() {
         return first == null;
     }
-
-   /**
-     * Return the number of items in the bag.
-     */
+/**.
+ * { function_description }
+ *
+ * @return     { description_of_the_return_value }
+ */
     public int size() {
-        return N;
+        return N_1;
     }
 
    /**
      * Add the item to the bag.
      */
-    public void add(Item item) {
+    public void add(final Item item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
         first.next = oldfirst;
         N++;
     }
-   /**
-     * Return an iterator that iterates over the items in the bag.
-     */
+/**.
+ * { function_description }
+ *
+ * @return     { description_of_the_return_value }
+ */
     public Iterator<Item> iterator()  {
         return new ListIterator();
     }
-
-    // an iterator, doesn't implement remove() since it's optional
+/**.
+ * Class for list iterator.
+ */
     private class ListIterator implements Iterator<Item> {
         /**.
          * { var_description }
