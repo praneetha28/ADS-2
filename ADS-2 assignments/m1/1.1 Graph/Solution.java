8a511b017f29381a947ab7f2b35999afbc466658
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-29 20:36:11 +0530

    "ads2_m1_a1_1: 7/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m1/1.1 Graph/Solution.java
+++ ADS-2 assignments/m1/1.1 Graph/Solution.java
@@ -27,35 +27,38 @@ public final class Solution {
         String word = sc.nextLine();
         int n =  sc.nextInt();
         int m = sc.nextInt();
+        int magic = 100;
         sc.nextLine();
         switch (word) {
-	        case"List":
-                if (n == 0 && m == 0 ) {
+            case"List":
+                if (n == 0 && m == 0) {
                     System.out.println(n + " vertices, " + m + " edges");
                     System.out.println("No edges");
                     break;
                 }
-    	        String vtexes = sc.nextLine();
-    	        String[] tokens1 = vtexes.split(",");
-    	        GraphList gl = new GraphList(n);
-    	        for (int i = 0; i < n; i++) {
-    	        	gl.addVertex(tokens1[i]);
-    	        }
+                String vtexes = sc.nextLine();
+                String[] tokens1 = vtexes.split(",");
+                GraphList gl = new GraphList(n);
+                for (int i = 0; i < n; i++) {
+                    gl.addVertex(tokens1[i]);
+                }
                 String line1;
-                String[] tokens2 = new String[100];
-    	        for (int i = 0; i < m; i++) {
-    	        	line1 = sc.nextLine();
-    	        	tokens2 = line1.split(" ");
-                        gl.addEdge(Integer.parseInt(tokens2[0]), Integer.parseInt(tokens2[1]));
-    	        }
-    	       	if (Integer.parseInt(tokens2[0]) == Integer.parseInt(tokens2[1])) {
+                String[] tokens2 = new String[magic];
+                for (int i = 0; i < m; i++) {
+                    line1 = sc.nextLine();
+                    tokens2 = line1.split(" ");
+                        gl.addEdge(Integer.parseInt(tokens2[0]),
+                         Integer.parseInt(tokens2[1]));
+                }
+                if (Integer.parseInt(tokens2[0]) ==
+                 Integer.parseInt(tokens2[1])) {
                     break;
                 } else {
                     System.out.println(gl.toString());
                 }
                 break;
             case"Matrix":
-                if (n == 0 && m == 0 ) {
+                if (n == 0 && m == 0) {
                     System.out.println(n + " vertices, " + m + " edges");
                     System.out.println("No edges");
                     break;
@@ -67,20 +70,22 @@ public final class Solution {
                     gm.addVertex(tkens[i]);
                 }
                 String line;
-                String[] tokens = new String[100];
+                String[] tokens = new String[magic];
                 for (int i = 0; i < m; i++) {
                     line = sc.nextLine();
                     tokens = line.split(" ");
-                    gm.addEdge(Integer.parseInt(tokens[0]), Integer.parseInt(tokens[1]));
+                    gm.addEdge(Integer.parseInt(tokens[0]),
+                     Integer.parseInt(tokens[1]));
                 }
-                if (Integer.parseInt(tokens[0]) == Integer.parseInt(tokens[1])) {
+                if (Integer.parseInt(tokens[0]) ==
+                 Integer.parseInt(tokens[1])) {
                     break;
                 } else {
                     System.out.println(gm.toString());
                 }
                 break;
-	        default:
-	        break;
-	    }
+            default:
+            break;
+        }
     }
 }
