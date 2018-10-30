0ff86de9d79c731f0b949074fc0c8dafcf1fb7e9
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-30 16:14:29 +0530

    "ads2_m2_a1_1: 2/24 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m2/2.1 CC_Percolation/Percolation.java
+++ ADS-2 assignments/m2/2.1 CC_Percolation/Percolation.java
@@ -8,33 +8,32 @@ class Percolation {
 		ghl = new GraphList((sze * sze) + 2);
 		grid = new boolean[size][size];
 	}
-	public boolean isOpen(int r, int c) {
-		return grid[r][c];
-	}
+	// public boolean isOpen(int r, int c) {
+	// 	return grid[r][c];
+	// }
 	public int getIndex(int i, int j) {
 		return (i*size) + j;
 	}
 	public void open(int r, int c) {
-		if (isOpen(r, c) == false) {
+
 			grid[r][c] = true;
 			count += 1;
-		}
 		if (r == 0) {
 			ghl.addEdge(size * size, getIndex(r, c));
 		}
 		if (r == size - 1) {
 			ghl.addEdge((size * size) + 1, getIndex(r, c));
 		}
-        if (c < size - 1 && isOpen(r, c + 1)) { //bottom
+        if (c < size - 1 && grid[r][c + 1]) { //bottom
         	ghl.addEdge(getIndex(r, c), getIndex(r, c + 1) );
         }
-        if (c > 0 && isOpen(r, c - 1)) { // left
+        if (c > 0 && grid[r][c - 1]) { // left
         	ghl.addEdge(getIndex(r, c), getIndex(r, c - 1) );
         }
-        if ( r < size - 1 && isOpen(r + 1, c)) { //right
+        if ( r < size - 1 && grid[r + 1][c]) { //right
         	ghl.addEdge(getIndex(r, c), getIndex(r + 1, c) );
         }
-        if (r > 0 && isOpen(r - 1, c)) { // top
+        if (r > 0 && grid[r - 1][c]) { // top
         	ghl.addEdge(getIndex(r, c), getIndex(r - 1, c) );
         }
 	}
