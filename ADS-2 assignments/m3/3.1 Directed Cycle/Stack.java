13cdcf6a9a8ed16895aefac054c5ec322f5fe749
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-31 13:47:07 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m3/3.1 Directed Cycle/Stack.java
+++ ADS-2 assignments/m3/3.1 Directed Cycle/Stack.java
@@ -15,7 +15,7 @@ public class Stack<Item> implements Iterable<Item> {
     /**.
      * { var_description }
      */
-    private int N;
+    private int n;
     /**.
      * { var_description }
      */
@@ -38,7 +38,7 @@ public class Stack<Item> implements Iterable<Item> {
      */
     public Stack() {
         first = null;
-        N = 0;
+        n = 0;
     }
     /**.
      * Determines if empty.
@@ -54,19 +54,19 @@ public class Stack<Item> implements Iterable<Item> {
      * @return     { description_of_the_return_value }
      */
     public int size() {
-        return N;
+        return n;
     }
     /**.
      * { function_description }
      *
      * @param      item  The item
      */
-    public void push(Item item) {
+    public void push(final Item item) {
         Node oldfirst = first;
         first = new Node();
         first.item = item;
         first.next = oldfirst;
-        N++;
+        n++;
     }
     /**.
      * { function_description }
@@ -74,10 +74,12 @@ public class Stack<Item> implements Iterable<Item> {
      * @return     { description_of_the_return_value }
      */
     public Item pop() {
-        if (isEmpty()) throw new RuntimeException("Stack underflow");
+        if (isEmpty()) {
+            throw new RuntimeException("Stack underflow");
+        }
         Item item = first.item;        // save item to return
         first = first.next;            // delete first node
-        N--;
+        n--;
         return item;                   // return the saved item
     }
     /**.
@@ -86,7 +88,9 @@ public class Stack<Item> implements Iterable<Item> {
      * @return     { description_of_the_return_value }
      */
     public Item peek() {
-        if (isEmpty()) throw new RuntimeException("Stack underflow");
+        if (isEmpty()) {
+            throw new RuntimeException("Stack underflow");
+        }
         return first.item;
     }
     /**.
@@ -96,8 +100,9 @@ public class Stack<Item> implements Iterable<Item> {
      */
     public String toString() {
         StringBuilder s = new StringBuilder();
-        for (Item item : this)
+        for (Item item : this) {
             s.append(item + " ");
+        }
         return s.toString();
     }
     /**.
