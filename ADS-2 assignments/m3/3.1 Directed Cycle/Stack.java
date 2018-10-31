b1055af3da4f9a39f415e825c56f444ee3808f29
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-31 13:48:09 +0530

    "ads2_m3_a1_1: 8/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m3/3.1 Directed Cycle/Stack.java
+++ ADS-2 assignments/m3/3.1 Directed Cycle/Stack.java
@@ -141,7 +141,9 @@ public class Stack<Item> implements Iterable<Item> {
          * @return     { description_of_the_return_value }
          */
         public Item next() {
-            if (!hasNext()) throw new NoSuchElementException();
+            if {
+                (!hasNext()) throw new NoSuchElementException();
+            }
             Item item = current.item;
             current = current.next;
             return item;
