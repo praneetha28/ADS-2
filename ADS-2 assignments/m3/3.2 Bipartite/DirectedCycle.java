33c8c116eaedc8e93d604f6f0514741217f137d7
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-10-31 17:19:06 +0530

    "ads2_m3_a2_1: 0/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

---
+++ ADS-2 assignments/m3/3.2 Bipartite/DirectedCycle.java
@@ -0,0 +1,84 @@
+/**.
+ * Class for directed cycle.
+ */
+public class DirectedCycle {
+    /**.
+     * { var_description }
+     */
+    private boolean[] marked;
+    /**.
+     * { var_description }
+     */
+    private int[] edgeTo;
+    /**.
+     * { var_description }
+     */
+    private boolean[] onStack;
+    private boolean isBipartite = true;
+    /**.
+     * { var_description }
+     */
+    private Stack<Integer> cycle;
+    /**.
+     * Determines whether the digraph has a directed cycle and, if so,
+     * finds such a cycle.
+     * @param g the digraph
+     */
+    public DirectedCycle(final Graph g) {
+        marked  = new boolean[g.V()];
+        onStack = new boolean[g.V()];
+        edgeTo  = new int[g.V()];
+        for (int v = 0; v < g.V(); v++) {
+            if (!marked[v] && cycle == null) {
+                dfs(g, v);
+            }
+        }
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      g     { parameter_description }
+     * @param      v     { parameter_description }
+     */
+    private void dfs(final Graph g, final int v) {
+        isBipartite = !isBipartite;
+        onStack[v] = true;
+        marked[v] = true;
+        for (int w : g.adj(v)) {
+            if (cycle != null) {
+                return;
+            } else if (!marked[w]) {
+                edgeTo[w] = v;
+                dfs(g, w);
+            } else if (onStack[w]) {
+                cycle = new Stack<Integer>();
+                for (int x = v; x != w; x = edgeTo[x]) {
+                    cycle.push(x);
+                }
+                cycle.push(w);
+                cycle.push(v);
+                // assert check();
+            }
+        }
+        onStack[v] = false;
+    }
+    /**.
+     * Determines if it has cycle.
+     *
+     * @return     True if has cycle, False otherwise.
+     */
+    public boolean hasCycle() {
+        return cycle != null;
+    }
+    public boolean isBipartite() {
+        return isBipartite;
+    }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Integer> cycle() {
+        return cycle;
+    }
+}
---
+++ ADS-2 assignments/m3/3.2 Bipartite/Solution.java
@@ -0,0 +1,42 @@
+/**.
+ * { item_description }
+ */
+import java.util.Scanner;
+/**.
+ * { item_description }
+ */
+public final class Solution {
+    /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      args  The arguments
+     */
+    // time complexity for the main method is O(N).
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        sc.nextLine();
+        Graph g = new Graph(n);
+        while (sc.hasNext()) {
+            String line = sc.nextLine();
+            String[] tokens = line.split(" ");
+            g.addEdge(Integer.parseInt(tokens[0]),
+                         Integer.parseInt(tokens[1]));
+        }
+        DirectedCycle dc = new DirectedCycle(g);
+        if (dc.isBipartite()) {
+            System.out.println("Graph is Bipartite");
+        } else {
+            System.out.println("Graph is not a Bipartite");
+        }
+    }
+}
