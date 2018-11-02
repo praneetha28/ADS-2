4b75f6e7ac6f6079f4f25d4198da479a30ca32a5
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-02 18:37:24 +0530

    "ads2_m4_a1_1: 15/15 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m4/Code Camp - WordNet/WordNet/Solution.java
+++ ADS-2 assignments/m4/Code Camp - WordNet/WordNet/Solution.java
@@ -26,15 +26,11 @@ public final class Solution {
         String m = "Files" + "\\" + sc.nextLine();
         String word = sc.nextLine();
         try {
-            // System.out.println("in try");
             WordNet wn = new WordNet(n, m);
             if (word.equals("Graph")) {
                 wn.display();
-                // System.out.println("display");
             } else if (word.equals("Queries")) {
-                // System.out.println("in queries");
                 while (sc.hasNextLine()) {
-                    // System.out.println("inn while");
                     String[] tokens = sc.nextLine().split(" ");
                     String str = wn.sap(tokens[0], tokens[1]);
                     int id = wn.distance(tokens[0], tokens[1]);
