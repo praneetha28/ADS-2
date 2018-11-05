81fad51e56b2727a3a832af4af71cb51721028c8
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-05 18:41:01 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m7/1. MST/Edge.java
+++ ADS-2 assignments/m7/1. MST/Edge.java
@@ -2,74 +2,74 @@
  * Class for edge.
  */
 class Edge implements Comparable<Edge> {
-	/**.
-	 * { var_description }
-	 */
-	private int v;
-	/**.
-	 * { var_description }
-	 */
-	private int w;
-	/**.
-	 * { var_description }
-	 */
-	private double weight;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      ver   The version
-	 * @param      edg   The edg
-	 * @param      wt    { parameter_description }
-	 */
-	Edge(final int ver, final int edg, final double wt) {
-		this.v = ver;
-		this.w = edg;
-		this.weight = wt;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public double weight() {
-		return weight;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int either() {
-		return v;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      ver   The version
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int other(final int ver) {
-		if (v == ver) {
-			return w;
-		} else {
-			return v;
-		}
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      that  The that
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-    public int compareTo(Edge that) {
-        if (this.weight < that.weight) {
-        	return -1;
-        } else if (this.weight > that.weight) {
-        	return 1;
+    /**.
+     * { var_description }
+     */
+    private int v;
+    /**.
+     * { var_description }
+     */
+    private int w;
+    /**.
+     * { var_description }
+     */
+    private double weight;
+    /**.
+     * Constructs the object.
+     *
+     * @param      ver   The version
+     * @param      edg   The edg
+     * @param      wt    { parameter_description }
+     */
+    Edge(final int ver, final int edg, final double wt) {
+        this.v = ver;
+        this.w = edg;
+        this.weight = wt;
+    }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public double weight() {
+        return weight;
+    }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int either() {
+        return v;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      ver   The version
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int other(final int ver) {
+        if (v == ver) {
+            return w;
         } else {
-        	return 0;
+            return v;
+        }
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Edge that) {
+        if (this.weight < that.weight) {
+            return -1;
+        } else if (this.weight > that.weight) {
+            return 1;
+        } else {
+            return 0;
         }
     }
 }
--- ADS-2 assignments/m7/1. MST/EdgeWeightedGraph.java
+++ ADS-2 assignments/m7/1. MST/EdgeWeightedGraph.java
@@ -2,70 +2,70 @@
  * Class for edge weighted graph.
  */
 class EdgeWeightedGraph {
-	/**.
-	 * { var_description }
-	 */
-	private int ver;
-	/**.
-	 * { var_description }
-	 */
-	private int edg;
-	/**.
-	 * { var_description }
-	 */
-	private double weight;
-	/**.
-	 * { var_description }
-	 */
-	private Bag<Edge>[] adj;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      v     { parameter_description }
-	 */
-	EdgeWeightedGraph(final int v) {
-		this.ver = v;
-		adj = (Bag<Edge>[]) new Bag[v];
+    /**.
+     * { var_description }
+     */
+    private int ver;
+    /**.
+     * { var_description }
+     */
+    private int edg;
+    /**.
+     * { var_description }
+     */
+    private double weight;
+    /**.
+     * { var_description }
+     */
+    private Bag<Edge>[] adj;
+    /**.
+     * Constructs the object.
+     *
+     * @param      v     { parameter_description }
+     */
+    EdgeWeightedGraph(final int v) {
+        this.ver = v;
+        adj = (Bag<Edge>[]) new Bag[v];
         for (int i = 0; i < v; i++) {
             adj[i] = new Bag<Edge>();
         }
-	}
-	/**.
-	 * Adds an edge.
-	 *
-	 * @param      e     { parameter_description }
-	 */
-	public void addEdge(final Edge e) {
-		int v = e.either();
-		int w = e.other(v);
-		adj[v].add(e);
-		adj[w].add(e);
-		edg++;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int vertices() {
-		return ver;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int edg() {
-		return edg;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      v     { parameter_description }
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public Iterable<Edge> adj(final int v) {
+    }
+    /**.
+     * Adds an edge.
+     *
+     * @param      e     { parameter_description }
+     */
+    public void addEdge(final Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+        edg++;
+    }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int vertices() {
+        return ver;
+    }
+    /**.
+     * { function_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int edg() {
+        return edg;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Edge> adj(final int v) {
         // validateVertex(v);
         return adj[v];
     }
@@ -77,7 +77,7 @@ class EdgeWeightedGraph {
      * @return     { description_of_the_return_value }
      */
     public int degree(final int v) {
-    	return adj[v].size();
+        return adj[v].size();
     }
     /**.
      * { function_description }
@@ -91,10 +91,10 @@ class EdgeWeightedGraph {
             for (Edge ed : adj(i)) {
                 if (ed.other(i) > i) {
                     list.add(ed);
-                }
-                // add only one copy of each self loop (self loops will be consecutive)
-                else if (ed.other(i) == i) {
-                    if (selfLoops % 2 == 0) list.add(ed);
+                } else if (ed.other(i) == i) {
+                    if (selfLoops % 2 == 0) {
+                        list.add(ed);
+                    }
                     selfLoops++;
                 }
             }
