1c793a1a78cbedb4596385c8ccfebd9c866be825
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-05 18:13:30 +0530

    "ads2_m7_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m7/1. MST/Solution.java
+++ ADS-2 assignments/m7/1. MST/Solution.java
@@ -29,9 +29,6 @@ public final class Solution {
         for (int i = 0; i < edges; i++) {
             String line = sc.nextLine();
             String[] tokens = line.split(" ");
-            // System.out.println(Integer.parseInt(tokens[0]));
-            // System.out.println(Integer.parseInt(tokens[1]));
-            // System.out.println(Double.parseDouble(tokens[2]));
             edwgh.addEdge(new Edge(Integer.parseInt(tokens[0]),
              Integer.parseInt(tokens[1]), Double.parseDouble(tokens[2])));
         }
