b1f6ae5ade4ee5aa4a188e7b68a6498ce886ea89
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-05 11:38:10 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 assignments/m6/ADS-2Week-1/PageRank.java
+++ ADS-2 assignments/m6/ADS-2Week-1/PageRank.java
@@ -14,8 +14,8 @@ class PageRank {
     /**.
      * { var_description }
      */
-    private double[] pr;
-    private double[] pr1;
+    private double[] prvalues;
+    // private double[] pr1;
     private int vertices;
     /**.
      * { var_description }
@@ -32,64 +32,64 @@ class PageRank {
      */
     PageRank(final Digraph dig) {
         dg = dig;
-        vertices = dig.vert();
-        pr = new double[vertices];
-        pr1 = new double[vertices];
+        // vertices = dig.vert();
+        prvalues = new double[vertices];
+        // pr1 = new double[vertices];
         for (int i = 0; i < vertices; i++) {
-            pr[i] = (1 / ((double) vertices));
+            prvalues[i] = (1 / ((double) dig.vert()));
         }
-        Digraph revdigraph = dig.reverse();
-        for (int i = 0; i < 1000; i++) {
-            for (int j = 0; j < vertices; j++) {
-                double result = 0.0;
-                // System.out.println("helloS");
-                for (int k : revdigraph.adj(j)) {
-                    result += ((pr[k]) / ((double) (dig.outdegree(k))));
-                    // System.out.println(result);
-                }
+        calculatePR();
+        // Digraph revdigraph = dig.reverse();
+        // for (int i = 0; i < 1000; i++) {
+        //     for (int j = 0; j < vertices; j++) {
+        //         double result = 0.0;
+        //         // System.out.println("helloS");
+        //         for (int k : revdigraph.adj(j)) {
+        //             result += ((pr[k]) / ((double) (dig.outdegree(k))));
+        //             // System.out.println(result);
+        //         }
 
-                pr1[j] = result;
-            }
-            if (Arrays.equals(pr, pr1)) {
-                break;
-            } else {
-                pr = pr1.clone();
-            }
-        }
-        for (int i = 0; i < vertices; i++) {
-            System.out.print(i + " - " + pr1[i] + "\n");
+        //         pr1[j] = result;
+        //     }
+        //     if (Arrays.equals(pr, pr1)) {
+        //         break;
+        //     } else {
+        //         pr = pr1.clone();
+        //     }
+        // }
+        // for (int i = 0; i < vertices; i++) {
+        //     System.out.print(i + " - " + pr1[i] + "\n");
+        // }
+    }
+    public void calculatePR() {
+    	double[] temppr = new double[dg.vert()];
+		Digraph revdg = dg.reverse();
+        for (int i = 0; i < 1000; i++) {
+			for (int j = 0; j < dg.vert(); j++) {
+				Double rank = 0.0;
+				for (int k : revdg.adj(j)) {
+					rank += prvalues[k] / (double) (dg.outdegree(k));
+				}
+				temppr[i] = rank;
+			}
+			if (Arrays.equals(prvalues, temppr)) {
+				break;
+			} else {
+				prvalues = temppr;
+			}
+		}
+    }
+    public Double getPR(int v) {
+    	return prvalues[v];
+    }
+    /**.
+     * { function_description }
+     */
+    public void string() {
+        for (int i = 0; i < prvalues.length; i++) {
+            String s = "";
+            s += String.valueOf(i) + " - " + prvalues[i];
+            System.out.println(s);
         }
     }
 }
-//     public void calculatePR() {
-//     	double[] temppr = new double[dg.vert()];
-// 		Digraph revdg = dg.reverse();
-//         for (int i = 0; i < 1000; i++) {
-// 			for (int j = 0; j < dg.vert(); j++) {
-// 				Double rank = 0.0;
-// 				for (int k : revdg.adj(j)) {
-// 					rank += prvalues[k] / (double) (dg.outdegree(k));
-// 				}
-// 				temppr[i] = rank;
-// 			}
-// 			if (Arrays.equals(prvalues, temppr)) {
-// 				break;
-// 			} else {
-// 				prvalues = temppr;
-// 			}
-// 		}
-//     }
-//     public Double getPR(int v) {
-//     	return prvalues[v];
-//     }
-//     /**.
-//      * { function_description }
-//      */
-//     public void string() {
-//         for (int i = 0; i < prvalues.length; i++) {
-//             String s = "";
-//             s += String.valueOf(i) + " - " + prvalues[i];
-//             System.out.println(s);
-//         }
-//     }
-// }
