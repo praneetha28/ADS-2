1c1fce1f3be6dcbc342b4a4dc2fc7fb6b151037d
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-05 08:34:57 +0530

    "ads2_m6_a1_1: 3/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 assignments/m6/ADS-2Week-1/PageRank.java
+++ ADS-2 assignments/m6/ADS-2Week-1/PageRank.java
@@ -33,16 +33,22 @@ class PageRank {
         for (int i = 0; i < dg.vert(); i++) {
             prvalues[i] = 1.0 / (dg.vert());
         }
+        for (int i = 0; i < dg.vert(); i++) {
+        	if (dg.outdegree(i) == 0) {
+        		for (int j = 0; j < dg.vert(); j++) {
+        			if (i != j) {
+        				dg.addEdge(i, j);
+        			}
+        		}
+        	}
+        }
         for (int i = 0; i < IT_ER; i++) {
             for (int j = 0; j < dg.vert(); j++) {
                 double temp = 0.0;
-                for (int k = 0; k < dg.vert(); k++) {
-                	for (int h : dg.adj(k)) {
-		                if (h == j) {
+                	for (int k : dg.adj(j)) {
+
 		                	temp += (double) (prvalues[k] / dg.outdegree(k));
-		                }
 	                }
-                }
 
                 prvalues[j] = temp;
             }
