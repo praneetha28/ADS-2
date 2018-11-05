d11cb63923e9918d79a5a30757e619edbb840788
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-05 08:53:49 +0530

    "ads2_m6_a1_1: 0/12 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 assignments/m6/ADS-2Week-1/PageRank.java
+++ ADS-2 assignments/m6/ADS-2Week-1/PageRank.java
@@ -30,32 +30,34 @@ class PageRank {
     PageRank(final Digraph dig) {
         dg = dig;
         prvalues = new double[dg.vert()];
-        for (int i = 0; i < dg.vert(); i++) {
-            prvalues[i] = 1.0 / (dg.vert());
-        }
-        for (int i = 0; i < dg.vert(); i++) {
-        	if (dg.outdegree(i) == 0) {
-        		for (int j = 0; j < dg.vert(); j++) {
-        			if (i != j) {
-        				dg.addEdge(i, j);
-        			}
-        		}
-        	}
-        }
-        for (int i = 0; i < IT_ER; i++) {
-            for (int j = 0; j < dg.vert(); j++) {
-                double temp = 0.0;
-                // for (int k = 0; k < dg.vert(); k++) {
-                	for (int h : dg.adj(j)) {
-		                // if (h == j) {
-		                	temp += (double) (prvalues[h] / dg.outdegree(h));
-		                }
-	                // }
-                // }
+        for (int l = 0; l < 1000; l++) {
+			// Double[] tempArray = new Double[digraph.V()];
+			//System.out.println("first for loop");
 
-                prvalues[j] = temp;
-            }
-        }
+			for (int i = 0; i < dg.vert(); i++) {
+				if (dg.outdegree(i) == 0) {
+					for (int j = 0; j < dg.vert(); j++) {
+						if(i!=j){
+						dg.addEdge(i, j);
+						}
+					}
+				}
+				Double rank = 0.0;
+				//for (int j = 0; j < digraph.V(); j++) {
+				//System.out.println("second for loop");
+
+				for (int k : dg.reverse().adj(i)) {
+					//if (k == i) {
+					//if (digraph.outdegree(j) > 0) {
+					rank += prvalues[k] / dg.outdegree(k);
+					//	}
+					//}
+					//}
+				}
+				prvalues[i] = rank;
+			}
+
+		}
 
     }
     public Double getPR(int v) {
