af98a9eefa50966041c298f07c41412a24a13b16
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-05 20:32:31 +0530

    "ads2_m7_a1_1: 3/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 assignments/m7/1. MST/MinPQ.java
+++ ADS-2 assignments/m7/1. MST/MinPQ.java
@@ -256,7 +256,7 @@ public class MinPQ<Key> implements Iterable<Key> {
     /**.
      * Class for heap iterator.
      */
-    private class HeapIterator implements Iterator<Key> {
+    private final class HeapIterator implements Iterator<Key> {
         /**.
          * { var_description }
          */
