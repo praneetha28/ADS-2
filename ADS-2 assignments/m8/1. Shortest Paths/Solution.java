1b3dbb89e054a301a4381086c28349d9beb34a87
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-08 17:08:28 +0530

    "ads2_m8_a1_1: 0/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 assignments/m8/1. Shortest Paths/Solution.java
+++ ADS-2 assignments/m8/1. Shortest Paths/Solution.java
@@ -26,21 +26,20 @@ public final class Solution {
     // time complexity for the main method is E.
     public static void main(final String[] args) {
         Scanner sc = new Scanner(System.in);
-        ArrayList<String> stations= new ArrayList<>();
-        String line = sc.nextLine();
-        String[] tokens = line.split(" ");
-        int vertices = Integer.parseInt(tokens[0]);
-        int edges = Integer.parseInt(tokens[1]);
-        String[]stnames = sc.nextLine().split(" ");
-        for (int i = 0 ; i < stnames.length; i++) {
-        	stations.add(stnames[i]);
+        ArrayList<String> stations = new ArrayList<>();
+        String[] line = sc.nextLine().split(" ");
+        String[] stnames = sc.nextLine().split(" ");
+        for (int i = 0; i < stnames.length; i++) {
+            stations.add(stnames[i]);
         }
-        EdgeWeightedDiGraph edwdgh = new EdgeWeightedDiGraph(vertices);
-        for (int i = 0; i < edges; i++) {
+        EdgeWeightedDiGraph edwdgh = new EdgeWeightedDiGraph(Integer.parseInt(line[0]));
+        for (int i = 0; i < Integer.parseInt(line[1]); i++) {
             String line1 = sc.nextLine();
             String[] tokens1 = line1.split(" ");
             edwdgh.addEdge(new DirectedEdge(stations.indexOf(tokens1[0]),
              stations.indexOf(tokens1[1]), Double.parseDouble(tokens1[2])));
+            edwdgh.addEdge(new DirectedEdge(stations.indexOf(tokens1[1]),
+             stations.indexOf(tokens1[0]), Double.parseDouble(tokens1[2])));
         }
         int query = sc.nextInt();
         for (int i = 0; i < query; i++) {
