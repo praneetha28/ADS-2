a5ac13c2ceffbdcb126ca963cd25d2e55fb4c1c7
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-08 17:22:03 +0530

    "ads2_m8_a1_1: 6/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 1/1 "

--- ADS-2 assignments/m8/1. Shortest Paths/Dijkstras.java
+++ ADS-2 assignments/m8/1. Shortest Paths/Dijkstras.java
@@ -2,28 +2,30 @@
  * Class for dijkstras.
  */
 class Dijkstras {
-	/**.
-	 * { var_description }
-	 */
-	private double[] distTo;
-	/**.
-	 * { var_description }
-	 */
-	private DirectedEdge[] edgeTo;
-	/**.
-	 * { var_description }
-	 */
-	private MinPQ<Double> pq;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      g     { parameter_description }
-	 * @param      s     { parameter_description }
-	 */
-	Dijkstras(final EdgeWeightedDiGraph g, final int s) {
-		for (DirectedEdge e : g.edges()) {
-            if (e.weight() < 0)
-                throw new IllegalArgumentException("edge " + e + " has negative weight");
+    /**.
+     * { var_description }
+     */
+    private double[] distTo;
+    /**.
+     * { var_description }
+     */
+    private DirectedEdge[] edgeTo;
+    /**.
+     * { var_description }
+     */
+    private MinPQ<Double> pq;
+    /**.
+     * Constructs the object.
+     *
+     * @param      g     { parameter_description }
+     * @param      s     { parameter_description }
+     */
+    Dijkstras(final EdgeWeightedDiGraph g, final int s) {
+        for (DirectedEdge e : g.edges()) {
+            if (e.weight() < 0) {
+                throw new IllegalArgumentException
+                ("edge " + e + " has negative weight");
+            }
         }
 
         distTo = new double[g.vertices()];
@@ -36,32 +38,59 @@ class Dijkstras {
         pq.insert(s, distTo[s]);
         while (!pq.isEmpty()) {
             int v = pq.delMin();
-            for (DirectedEdge e : g.adj(v))
+            for (DirectedEdge e : g.adj(v)) {
                 relax(e);
+            }
         }
-	}
-	public void relax(final DirectedEdge e) {
-		int v = e.from();
-		int w = e.to(v);
-		if (distTo[w] > distTo[v] + e.weight()) {
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      e     { parameter_description }
+     */
+    public void relax(final DirectedEdge e) {
+        int v = e.from();
+        int w = e.to(v);
+        if (distTo[w] > distTo[v] + e.weight()) {
             distTo[w] = distTo[v] + e.weight();
             edgeTo[w] = e;
             if (pq.contains(w)) {
-            	pq.decreaseKey(w, distTo[w]);
+                pq.decreaseKey(w, distTo[w]);
             } else {
                 pq.insert(w, distTo[w]);
             }
         }
-	}
-	public double distTo(final int v) {
-		return distTo[v];
-	}
-	public boolean hasPathTo(final int v) {
-		return distTo[v] < Double.POSITIVE_INFINITY;
-	}
-	public Iterable<DirectedEdge> pathTo(final int v) {
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public double distTo(final int v) {
+        return distTo[v];
+    }
+    /**.
+     * Determines if it has path to.
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     True if has path to, False otherwise.
+     */
+    public boolean hasPathTo(final int v) {
+        return distTo[v] < Double.POSITIVE_INFINITY;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<DirectedEdge> pathTo(final int v) {
         if (!hasPathTo(v)) {
-        	return null;
+            return null;
         }
         Stack<DirectedEdge> path = new Stack<DirectedEdge>();
         for (DirectedEdge e = edgeTo[v]; e != null; e = edgeTo[e.from()]) {
