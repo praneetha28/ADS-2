f56ce541b01ba31e101d7e5ed0710e23725837f3
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-10 10:30:47 +0530

    "ads2_m12_a1_1: 2/6 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2 assignments/m12/ADS-2_Exam-2/Edge.java
@@ -0,0 +1,83 @@
+/**.
+ * Class for edge.
+ */
+class Edge implements Comparable<Edge> {
+    /**.
+     * { var_description }
+     */
+    private int v;
+    /**.
+     * { var_description }
+     */
+    private int w;
+    /**.
+     * { var_description }
+     */
+    private double weight;
+    /**.
+     * Constructs the object.
+     *
+     * @param      ver   The version
+     * @param      edg   The edg
+     * @param      wt    { parameter_description }
+     */
+    Edge(final int ver, final int edg, final double wt) {
+        this.v = ver;
+        this.w = edg;
+        this.weight = wt;
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @return     { description_of_the_return_value }
+     */
+    public double weight() {
+        return weight;
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @return     { description_of_the_return_value }
+     */
+    public int either() {
+        return v;
+    }
+    /**.
+     * { function_description }
+     *  TIME complexity in average case is 1.
+     * @param      ver   The version
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int other(final int ver) {
+        if (v == ver) {
+            return w;
+        } else {
+            return v;
+        }
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @param      that  The that
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int compareTo(final Edge that) {
+        if (this.weight < that.weight) {
+            return -1;
+        } else if (this.weight > that.weight) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        return String.format("%d-%d %.5f", v, w, weight);
+    }
+}
---
+++ ADS-2 assignments/m12/ADS-2_Exam-2/EdgeWeightedGraph.java
@@ -0,0 +1,121 @@
+/**.
+ * Class for edge weighted graph.
+ */
+class EdgeWeightedGraph {
+    /**.
+     * { var_description }
+     */
+    private int ver;
+    /**.
+     * { var_description }
+     */
+    private int edg;
+    /**.
+     * { var_description }
+     */
+    private double weight;
+    /**.
+     * { var_description }
+     */
+    private Bag<Edge>[] adj;
+    /**.
+     * Constructs the object.
+     *
+     * @param      v     { parameter_description }
+     */
+    EdgeWeightedGraph(final int v) {
+        this.ver = v;
+        adj = (Bag<Edge>[]) new Bag[v];
+        for (int i = 0; i < v; i++) {
+            adj[i] = new Bag<Edge>();
+        }
+    }
+    /**.
+     * Adds an edge.
+     * TIME complexity in average case is 1.
+     * @param      e     { parameter_description }
+     */
+    public void addEdge(final Edge e) {
+        int v = e.either();
+        int w = e.other(v);
+        adj[v].add(e);
+        adj[w].add(e);
+        edg++;
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @return     { description_of_the_return_value }
+     */
+    public int vertices() {
+        return ver;
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @return     { description_of_the_return_value }
+     */
+    public int edg() {
+        return edg;
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Edge> adj(final int v) {
+        // validateVertex(v);
+        return adj[v];
+    }
+    /**.
+     * { function_description }
+     * TIME complexity in average case is 1.
+     * @param      v     { parameter_description }
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int degree(final int v) {
+        return adj[v].size();
+    }
+    /**.
+     * { function_description }
+     * TIME complexity is no of edges.
+     * @return     { description_of_the_return_value }
+     */
+    public Iterable<Edge> edges() {
+        Bag<Edge> list = new Bag<Edge>();
+        for (int i = 0; i < ver; i++) {
+            int selfLoops = 0;
+            for (Edge ed : adj(i)) {
+                if (ed.other(i) > i) {
+                    list.add(ed);
+                } else if (ed.other(i) == i) {
+                    if (selfLoops % 2 == 0) {
+                        list.add(ed);
+                    }
+                    selfLoops++;
+                }
+            }
+        }
+        return list;
+    }
+    /**.
+     * Returns a string representation of the object.
+     *
+     * @return     String representation of the object.
+     */
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+        s.append(ver + " vertices " + edg + " edges" + "\n");
+        for (int v = 0; v < ver; v++) {
+            s.append(v + ": ");
+            for (Edge e : adj[v]) {
+                s.append(e + "  ");
+            }
+            s.append("\n");
+        }
+        return s.toString();
+    }
+}
