70e4fc3a3526ff7f3ca61a6c5515f247c1101ccb
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-12 13:46:31 +0530

    "ads2_m13_a1_1: 0/8 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

---
+++ ADS-2 assignments/m13/Radix Sorts/Solution.java
@@ -0,0 +1,34 @@
+/**.
+ * { item_description }
+ */
+import java.util.Scanner;
+/**.
+ * Class for solution.
+ */
+public final class Solution {
+ /**.
+     * Constructs the object.
+     */
+    private Solution() {
+        /**.
+         * { item_description }
+         */
+    }
+    /**.
+     * { function_description }
+     * the time complexity is E as it iterates through no of edges.
+     * @param      args  The arguments
+     */
+    public static void main(final String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt();
+        String[] rollnum = new String[num];
+        for (int i = 0; i <= num; i++) {
+            String line = sc.nextLine();
+            rollnum[i] = line;
+        }
+        Quick3string q3str = new Quick3string();
+        q3str.sort(rollnum);
+        System.out.println(q3str.display(rollnum));
+    }
+}
