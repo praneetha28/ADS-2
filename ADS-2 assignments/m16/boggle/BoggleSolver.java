a98028578e6d2f261ea1a9734271196bc980d865
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-16 10:06:52 +0530

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m16/boggle/BoggleSolver.java
+++ ADS-2 assignments/m16/boggle/BoggleSolver.java
@@ -6,10 +6,10 @@ import java.util.HashSet;
  * Class for boggle solver.
  */
 public class BoggleSolver {
-	/**.
-	 * { var_description }
-	 */
-	private TrieST trst;
+    /**.
+     * { var_description }
+     */
+    private TrieST trst;
     /**.
      * { var_description }
      */
@@ -34,109 +34,109 @@ public class BoggleSolver {
      * { var_description }
      */
     public static final int E_L_E = 11;
-	/**.
-	 * Constructs the object.
-	 *
-	 * @param      dictionary  The dictionary
-	 */
-	public BoggleSolver(final String[] dictionary) {
-		trst = new TrieST();
-		for (int i = 0; i < dictionary.length; i++) {
-			trst.add(dictionary[i]);
-		}
-	}
-	/**.
-	 * Gets all valid words.
-	 *
-	 * @param      board  The board
-	 *
-	 * @return     All valid words.
-	 */
-	public HashSet<String> getAllValidWords(final BoggleBoard board) {
-		HashSet<String> words = new HashSet<String>();
+    /**.
+     * Constructs the object.
+     *
+     * @param      dictionary  The dictionary
+     */
+    public BoggleSolver(final String[] dictionary) {
+        trst = new TrieST();
+        for (int i = 0; i < dictionary.length; i++) {
+            trst.add(dictionary[i]);
+        }
+    }
+    /**.
+     * Gets all valid words.
+     *
+     * @param      board  The board
+     *
+     * @return     All valid words.
+     */
+    public HashSet<String> getAllValidWords(final BoggleBoard board) {
+        HashSet<String> words = new HashSet<String>();
 
-		for (int i = 0; i < board.rows(); i++) {
-			for (int j = 0; j < board.cols(); j++) {
-				boolean[][] visited = new boolean[
-				board.rows()][board.cols()];
-				searchWord(board, i, j, "", visited, words);
-			}
+        for (int i = 0; i < board.rows(); i++) {
+            for (int j = 0; j < board.cols(); j++) {
+                boolean[][] visited = new boolean[
+                board.rows()][board.cols()];
+                searchWord(board, i, j, "", visited, words);
+            }
 
-		}
-		return words;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      board    The board
-	 * @param      i        { parameter_description }
-	 * @param      j        { parameter_description }
-	 * @param      str      The string
-	 * @param      visited  The visited
-	 * @param      words    The words
-	 */
-	private void searchWord(final BoggleBoard board, final int i,
-	    final int j, final String str, final boolean[][] visited,
-	                        final HashSet words) {
-		if (visited[i][j]) {
-			return;
-		}
-		char ch = board.getLetter(i, j);
-		String word = str;
-		if (ch == 'Q') {
-			word += "QU";
-		} else {
-			word += ch;
-		}
-		if (!trst.hasPrefix(word)) {
-			return;
-		}
-		if (word.length() > 2 && trst.contains(word)) {
-			words.add(word);
-		}
-		visited[i][j] = true;
-		for (int k = -1; k <= 1; k++) {
-			for (int l = -1; l <= 1; l++) {
-				if (k == 0 && l == 0) {
-					continue;
-				}
-				if ((i + k >= 0) && (i + k < board.rows())
-				        && (j + l >= 0) && (j + l < board.cols())) {
-					searchWord(board, i + k, j + l, word,
-					           visited, words);
-				}
-			}
-		}
-		visited[i][j] = false;
-	}
-	/**.
-	 * { function_description }
-	 *
-	 * @param      word  The word
-	 *
-	 * @return     { description_of_the_return_value }
-	 */
-	public int scoreOf(final String word) {
-		if (trst.contains(word)) {
-			switch (word.length()) {
-			case 0:
-			case 1:
-			case 2:
-				return 0;
-			case T_H_R_E:
-			case F_O_U_R:
-				return 1;
-			case F_I_V_E:
-				return 2;
-			case S_I_X:
-				return T_H_R_E;
-			case S_E_V_E_N:
-				return F_I_V_E;
-			default:
-				return E_L_E;
-			}
-		} else {
-			return 0;
-		}
-	}
+        }
+        return words;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      board    The board
+     * @param      i        { parameter_description }
+     * @param      j        { parameter_description }
+     * @param      str      The string
+     * @param      visited  The visited
+     * @param      words    The words
+     */
+    private void searchWord(final BoggleBoard board, final int i,
+        final int j, final String str, final boolean[][] visited,
+                            final HashSet words) {
+        if (visited[i][j]) {
+            return;
+        }
+        char ch = board.getLetter(i, j);
+        String word = str;
+        if (ch == 'Q') {
+            word += "QU";
+        } else {
+            word += ch;
+        }
+        if (!trst.hasPrefix(word)) {
+            return;
+        }
+        if (word.length() > 2 && trst.contains(word)) {
+            words.add(word);
+        }
+        visited[i][j] = true;
+        for (int k = -1; k <= 1; k++) {
+            for (int l = -1; l <= 1; l++) {
+                if (k == 0 && l == 0) {
+                    continue;
+                }
+                if ((i + k >= 0) && (i + k < board.rows())
+                        && (j + l >= 0) && (j + l < board.cols())) {
+                    searchWord(board, i + k, j + l, word,
+                               visited, words);
+                }
+            }
+        }
+        visited[i][j] = false;
+    }
+    /**.
+     * { function_description }
+     *
+     * @param      word  The word
+     *
+     * @return     { description_of_the_return_value }
+     */
+    public int scoreOf(final String word) {
+        if (trst.contains(word)) {
+            switch (word.length()) {
+            case 0:
+            case 1:
+            case 2:
+                return 0;
+            case T_H_R_E:
+            case F_O_U_R:
+                return 1;
+            case F_I_V_E:
+                return 2;
+            case S_I_X:
+                return T_H_R_E;
+            case S_E_V_E_N:
+                return F_I_V_E;
+            default:
+                return E_L_E;
+            }
+        } else {
+            return 0;
+        }
+    }
 }
