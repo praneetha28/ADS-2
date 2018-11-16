f8f539b99051318d8888476cbc983b431af67437
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-16 09:44:26 +0530

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m16/boggle/BoggleSolver.java
+++ ADS-2 assignments/m16/boggle/BoggleSolver.java
@@ -1,16 +1,34 @@
-import java.util.*;
+/**.
+ * { item_description }
+ */
+import java.util.HashSet;
+/**.
+ * Class for boggle solver.
+ */
 public class BoggleSolver {
-	// Initializes the data structure using the given array of strings as the dictionary.
-	// (You can assume each word in the dictionary contains only the uppercase letters A through Z.)
-	TrieST trst;
-	public BoggleSolver(String[] dictionary) {
+	/**.
+	 * { var_description }
+	 */
+	private TrieST trst;
+	/**.
+	 * Constructs the object.
+	 *
+	 * @param      dictionary  The dictionary
+	 */
+	public BoggleSolver(final String[] dictionary) {
 		trst = new TrieST();
 		for (int i = 0; i < dictionary.length; i++) {
-				trst.add(dictionary[i]);
+			trst.add(dictionary[i]);
 		}
 	}
-// Returns the set of all valid words in the given Boggle board, as an Iterable.
-	public HashSet<String> getAllValidWords(BoggleBoard board) {
+	/**.
+	 * Gets all valid words.
+	 *
+	 * @param      board  The board
+	 *
+	 * @return     All valid words.
+	 */
+	public HashSet<String> getAllValidWords(final BoggleBoard board) {
 		HashSet<String> words = new HashSet<String>();
 
 		for (int i = 0; i < board.rows(); i++) {
@@ -22,13 +40,25 @@ public class BoggleSolver {
 		}
 		return words;
 	}
-	private void searchWord(BoggleBoard board, int i, int j, String str, boolean[][] visited, HashSet words) {
+	/**.
+	 * { function_description }
+	 *
+	 * @param      board    The board
+	 * @param      i        { parameter_description }
+	 * @param      j        { parameter_description }
+	 * @param      str      The string
+	 * @param      visited  The visited
+	 * @param      words    The words
+	 */
+	private void searchWord(final BoggleBoard board, final int i,
+	                        final int j, final String str, final boolean[][] visited,
+	                        final HashSet words) {
 		if (visited[i][j]) {
 			return;
 		}
 		char ch = board.getLetter(i, j);
 		String word = str;
-		if (ch =='Q') {
+		if (ch == 'Q') {
 			word += "QU";
 		} else {
 			word += ch;
@@ -41,40 +71,47 @@ public class BoggleSolver {
 		}
 		visited[i][j] = true;
 		for (int k = -1; k <= 1; k++) {
-            for (int l = -1; l <= 1; l++) {
-                if (k == 0 && l == 0) {
-                    continue;
-                }
-                if ((i + k >= 0) && (i + k < board.rows()) && (j + l >= 0) && (j + l < board.cols())) {
-                    searchWord(board, i + k, j + l, word, visited, words);
-                }
-            }
-        }
-        visited[i][j] = false;
+			for (int l = -1; l <= 1; l++) {
+				if (k == 0 && l == 0) {
+					continue;
+				}
+				if ((i + k >= 0) && (i + k < board.rows())
+				        && (j + l >= 0) && (j + l < board.cols())) {
+					searchWord(board, i + k, j + l, word,
+					           visited, words);
+				}
+			}
+		}
+		visited[i][j] = false;
 	}
-	// Returns the score of the given word if it is in the dictionary, zero otherwise.
-	// (You can assume the word contains only the uppercase letters A through Z.)
-	public int scoreOf(String word) {
+	/**.
+	 * { function_description }
+	 *
+	 * @param      word  The word
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public int scoreOf(final String word) {
 		if (trst.contains(word)) {
-            switch (word.length()) {
-            case 0:
-            case 1:
-            case 2:
-                return 0;
-            case 3:
-            case 4:
-                return 1;
-            case 5:
-                return 2;
-            case 6:
-                return 3;
-            case 7:
-                return 5;
-            default:
-                return 11;
-            }
-        } else {
-            return 0;
-        }
+			switch (word.length()) {
+			case 0:
+			case 1:
+			case 2:
+				return 0;
+			case 3:
+			case 4:
+				return 1;
+			case 5:
+				return 2;
+			case 6:
+				return 3;
+			case 7:
+				return 5;
+			default:
+				return 11;
+			}
+		} else {
+			return 0;
+		}
 	}
 }
