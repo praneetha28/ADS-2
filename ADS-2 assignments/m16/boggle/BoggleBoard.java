3973b8f4a65a824bbc0a7cecbb28ee26b7b6cf52
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-16 10:32:32 +0530

    "ads2_m16_a1_1: 30/30 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m16/boggle/BoggleBoard.java
+++ ADS-2 assignments/m16/boggle/BoggleBoard.java
@@ -70,8 +70,8 @@ public class BoggleBoard {
      * { var_description }
      */
     public BoggleBoard() {
-        m = 4;
-        n = 4;
+        m = 2 + 2;
+        n = 2 + 2;
         StdRandom.shuffle(BOGGLE_1992);
         board = new char[m][n];
         for (int i = 0; i < m; i++) {
@@ -223,42 +223,4 @@ public class BoggleBoard {
         }
         return sb.toString().trim();
     }
-
-    // /**
-    //  * Unit tests the BoggleBoard data type.
-    //  */
-    // public static void main(String[] args) {
-
-    //     // initialize a 4-by-4 board using Hasbro dice
-    //     StdOut.println("Hasbro board:");
-    //     BoggleBoard board1 = new BoggleBoard();
-    //     StdOut.println(board1);
-    //     StdOut.println();
-
-    //     // initialize a 4-by-4 board using letter frequencies in English language
-    //     StdOut.println("Random 4-by-4 board:");
-    //     BoggleBoard board2 = new BoggleBoard(4, 4);
-    //     StdOut.println(board2);
-    //     StdOut.println();
-
-    //     // initialize a 4-by-4 board from a 2d char array
-    //     StdOut.println("4-by-4 board from 2D character array:");
-    //     char[][] a =  {
-    //         { 'D', 'O', 'T', 'Y' },
-    //         { 'T', 'R', 'S', 'F' },
-    //         { 'M', 'X', 'M', 'O' },
-    //         { 'Z', 'A', 'B', 'W' }
-    //     };
-    //     BoggleBoard board3 = new BoggleBoard(a);
-    //     StdOut.println(board3);
-    //     StdOut.println();
-
-    //     // initialize a 4-by-4 board from a file
-    //     String filename = "board-quinquevalencies.txt";
-    //     StdOut.println("4-by-4 board from file " + filename + ":");
-    //     BoggleBoard board4 = new BoggleBoard(filename);
-    //     StdOut.println(board4);
-    //     StdOut.println();
-    // }
-
 }
