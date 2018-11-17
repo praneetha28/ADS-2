a1acefd8bac4a317aa8a5bc5324b4e2cb4833429
lakshmipraneetha4 <praneetha@LAPTOP-8KL032v4>
2018-11-17 12:09:50 +0530

    "ads2_m18_a1_1: 3/5 testcases passed. Cannot run hidden testcases as Server is unreachablecheck_style score: 0/1 "

--- ADS-2 assignments/m18/ADS-2Exam-3/Solution.java
+++ ADS-2 assignments/m18/ADS-2Exam-3/Solution.java
@@ -1,90 +1,120 @@
+/**.
+ * { item_description }
+ */
 import java.util.Scanner;
-
+/**.
+ * { item_description }
+ */
 import java.util.TreeSet;
-public class Solution {
+/**
+ * Class for solution.
+ */
+public final class Solution {
 
-	// Don't modify this method.
+    /**
+     * Constructs the object.
+     */
+    private Solution() {
+        // empty constructor
+    }
+
+    /**
+     * Main method.
+     *
+     * @param      args  The arguments
+     */
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		String cases = scan.nextLine();
 
 		switch (cases) {
-		case "loadDictionary":
-			// input000.txt and output000.txt
-			BinarySearchST<String, Integer> hash = loadDictionary("/Files/t9.csv");
-			while (scan.hasNextLine()) {
-				String key = scan.nextLine();
-				System.out.println(hash.get(key));
-			}
-			break;
+			case "loadDictionary":
+				// input000.txt and output000.txt
+				BinarySearchST<String, Integer> hash = loadDictionary("/Files/t9.csv");
+				while (scan.hasNextLine()) {
+					String key = scan.nextLine();
+					System.out.println(hash.get(key));
+				}
+				break;
 
-		case "getAllPrefixes":
-			// input001.txt and output001.txt
-			T9 t9 = new T9(loadDictionary("/Files/t9.csv"));
-			while (scan.hasNextLine()) {
-				String prefix = scan.nextLine();
-				for (String each : t9.getAllWords(prefix)) {
+			case "getAllPrefixes":
+				// input001.txt and output001.txt
+				T9 t9 = new T9(loadDictionary("/Files/t9.csv"));
+				while (scan.hasNextLine()) {
+					String prefix = scan.nextLine();
+					for (String each : t9.getAllWords(prefix)) {
+						System.out.println(each);
+					}
+				}
+				break;
+
+			case "potentialWords":
+				// input002.txt and output002.txt
+				t9 = new T9(loadDictionary("/Files/t9.csv"));
+				int count = 0;
+				while (scan.hasNextLine()) {
+					String t9Signature = scan.nextLine();
+					for (String each : t9.potentialWords(t9Signature)) {
+						count++;
+						System.out.println(each);
+					}
+				}
+				if (count == 0) {
+					System.out.println("No valid words found.");
+				}
+				break;
+
+			case "topK":
+				// input003.txt and output003.txt
+				t9 = new T9(loadDictionary("/Files/t9.csv"));
+				Bag<String> bag = new Bag<String>();
+				int k = Integer.parseInt(scan.nextLine());
+				while (scan.hasNextLine()) {
+					String line = scan.nextLine();
+					bag.add(line);
+				}
+				for (String each : t9.getSuggestions(bag, k)) {
 					System.out.println(each);
 				}
-			}
-			break;
 
-		case "potentialWords":
-			// input002.txt and output002.txt
-			t9 = new T9(loadDictionary("/Files/t9.csv"));
-			int count = 0;
-			while (scan.hasNextLine()) {
-				String t9Signature = scan.nextLine();
-				for (String each : t9.potentialWords(t9Signature)) {
-					count++;
-					System.out.println(each);
+				break;
+
+			case "t9Signature":
+				// input004.txt and output004.txt
+				t9 = new T9(loadDictionary("/Files/t9.csv"));
+				bag = new Bag<String>();
+				k = Integer.parseInt(scan.nextLine());
+				while (scan.hasNextLine()) {
+					String line = scan.nextLine();
+					for (String each : t9.t9(line, k)) {
+						System.out.println(each);
+					}
 				}
-			}
-			if (count == 0) {
-				System.out.println("No valid words found.");
-			}
-			break;
+				break;
 
-		case "topK":
-			// input003.txt and output003.txt
-			t9 = new T9(loadDictionary("/Files/t9.csv"));
-			Bag<String> bag = new Bag<String>();
-			int k = Integer.parseInt(scan.nextLine());
-			while (scan.hasNextLine()) {
-				String line = scan.nextLine();
-				bag.add(line);
-			}
-			for (String each : t9.getSuggestions(bag, k)) {
-				System.out.println(each);
-			}
-
-			break;
-
-		case "t9Signature":
-			// input004.txt and output004.txt
-			t9 = new T9(loadDictionary("/Files/t9.csv"));
-			bag = new Bag<String>();
-			k = Integer.parseInt(scan.nextLine());
-			while (scan.hasNextLine()) {
-				String line = scan.nextLine();
-				for (String each : t9.t9(line, k)) {
-					System.out.println(each);
-				}
-			}
-			break;
-
-		default:
-			break;
+			default:
+				break;
 
 		}
 	}
-
-	// Don't modify this method.
+	/**.
+	 * { function_description }
+	 *
+	 * @param      file  The file
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public static String[] toReadFile(String file) {
 		In in = new In(file);
 		return in.readAllStrings();
 	}
-
+	/**.
+	 * Loads a dictionary.
+	 *
+	 * @param      file  The file
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
 	public static BinarySearchST<String, Integer> loadDictionary(String file) {
 		BinarySearchST<String, Integer>  st = new BinarySearchST<String, Integer>();
 		// your code goes here
@@ -102,11 +132,24 @@ public class Solution {
 	}
 
 }
-
+/**.
+ * Class for t 9.
+ */
 class T9 {
+	/**.
+	 * { var_description }
+	 */
 	private BinarySearchST<String, Integer> bst;
+	/**.
+	 * { var_description }
+	 */
 	private TST<Integer> tst;
-	public T9(BinarySearchST<String, Integer> st) {
+	/**.
+	 * Constructs the object.
+	 *
+	 * @param      st    { parameter_description }
+	 */
+	public T9(final BinarySearchST<String, Integer> st) {
 		// your code goes here
 		bst = st;
 		tst = new TST<Integer>();
@@ -114,22 +157,36 @@ class T9 {
 			tst.put(key, bst.get(key));
 		}
 	}
-
-	// get all the prefixes that match with given prefix.
-	public Iterable<String> getAllWords(String prefix) {
-		// your code goes here
+	/**.
+	 * Gets all words.
+	 *
+	 * @param      prefix  The prefix
+	 *
+	 * @return     All words.
+	 */
+	public Iterable<String> getAllWords(final String prefix) {
 		return tst.keysWithPrefix(prefix);
-		// return null;
-	}
 
-	public Iterable<String> potentialWords(String t9Signature) {
-		// your code goes here
-		// return tst.keysThatMatch(t9Signature);
-		return null;
 	}
-
-	// return all possibilities(words), find top k with highest frequency.
-	public Iterable<String> getSuggestions(Iterable<String> words, int k) {
+	/**.
+	 * { function_description }
+	 *
+	 * @param      t9Signature  The t 9 signature
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public Iterable<String> potentialWords(final String t9Signature) {
+		return tst.keysThatMatch(t9Signature);
+	}
+	/**.
+	 * Gets the suggestions.
+	 *
+	 * @param      words  The words
+	 * @param      k      { parameter_description }
+	 *
+	 * @return     The suggestions.
+	 */
+	public Iterable<String> getSuggestions(final Iterable<String> words, final int k) {
 		TreeSet<String> wordList = new TreeSet<String>();
 		MaxPQ<Integer> valuemax = new MaxPQ<Integer>();
 		for (String word : words) {
@@ -145,10 +202,15 @@ class T9 {
 		}
 		return wordList;
 	}
-
-	// final output
-	// Don't modify this method.
-	public Iterable<String> t9(String t9Signature, int k) {
+	/**.
+	 * { function_description }
+	 *
+	 * @param      t9Signature  The t 9 signature
+	 * @param      k            { parameter_description }
+	 *
+	 * @return     { description_of_the_return_value }
+	 */
+	public Iterable<String> t9(final String t9Signature, final int k) {
 		return getSuggestions(potentialWords(t9Signature), k);
 	}
 }
